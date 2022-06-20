537ec1879a871e712d5377cf65282b96412b7254
RinaSh1 <92697073+RinaSh1@users.noreply.github.com>
2022-06-20 21:57:49 +0200

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Rina Shabani on 20.6.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
